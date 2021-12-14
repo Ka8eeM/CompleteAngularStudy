55ec1fb64016e1f88e46bd094bbc78388fc0e1ef
Karim Mohamed Abdelatif <karimabdelatif1@gmail.com>
2021-12-14 22:06:26 +0200

    Adding Sign-up and Sign-in page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    SignInComponent,
+    HomeComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
